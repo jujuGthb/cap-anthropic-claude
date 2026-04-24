629b8bb29bd611b5aaebbb5ecf7b3a9406426079
jujuGthb <julianaromialison3@gmail.com>
2026-04-24 17:00:07 +0300

    updated

--- src/executors/ObjectDetectionExecutor.py
+++ src/executors/ObjectDetectionExecutor.py
@@ -37,7 +37,7 @@ class ObjectDetectionExecutor(Capsule):
         self.extended_thinking = self.request.get_param("extendedThinking")
         self.thinking_budget_tokens = self.request.get_param("thinkingBudgetTokens")
         self.temperature = self.request.get_param("inputTemperature")
-        self.max_tokens = self.request.get_param("maxTokens") or 3000
+        self.max_tokens = self.request.get_param("maxTokens")
         self.max_concurrent_requests = self.request.get_param("maxConcurrentRequests")
         self.image_selector = self.request.get_param("inputImage")
 
